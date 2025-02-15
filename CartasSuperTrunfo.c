bd5c1326c505354696872eead30350c254b7a00f
waltembergcn <202409161364@alunos.estacio.br>
2025-02-15 09:35:56 +0000

    Desafio iniciante concluido

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -10,13 +10,37 @@ int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
+        float codigo_da_cidade;
+    char nome[25];
+    float populacao;
+    float area;
+    float PIB;
+    int n_pontos;
+    
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+
+    printf("Digite o código da cidade: \n");
+    scanf("%f", codigo_da_cidade);
+
+    printf("Digite o nome da cidade: \n");
+    scanf("%s", nome);
+
+    printf("Digite o número da população: \n");
+    scanf("%f", populacao);
+
+    printf("Digite area da cidade: \n");
+    scanf("%f", area);
+
+    printf("Digite o PIB da cidade: \n");
+    scanf("%f", PIB);
+
+    printf("Digite o numero de pontos da cidade: \n");
+    scanf("%d", n_pontos);
 
     return 0;
 }
---
+++ Referencias.C
@@ -0,0 +1,31 @@
+//Especificadores
+    //%d: Imprime um inteiro no formato decimal.
+    //%i: Equivalente a %d.
+    //%f: Imprime um número de ponto flutuante no formato padrão.
+    //%e: Imprime um número de ponto flutuante na notação científica.
+    //%c: Imprime um único caractere.
+    //%s: Imprime uma cadeia (string) de caracteres.
+
+    //Exemplo programa 1:
+
+    //int idade = 25;
+    //float altura = 1.75;
+    //double saldoBancario = 12345.67;
+    //char inicial = 'A';
+    //char nome[20] = "Bruno";
+ 
+    //printf("Idade: %d anos\n", idade);
+    //printf("Altura: %.2f metros\n", altura);
+    //printf("Saldo Bancário: %.2f reais\n", saldoBancario);
+    //printf("Inicial do Nome: %c\n", inicial);
+    //printf("Nome: %s\n", nome);
+ 
+    //return 0;
+
+    //Exemplo programa 2:
+
+    //int idade;
+    //printf("Digite sua idade: ");
+    //scanf("%d", &idade);
+    //printf("Sua idade é: %d\n", idade);
+    //return 0;
